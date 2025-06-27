e2d24ef7a392a72dc23e5fc6ba10dfb6b24358ea
Hema-devi <151372311+Hema-devi@users.noreply.github.com>
2025-06-27 20:50:20 +0530

    Create Class and Object.py

---
+++ 27.06.2025/Class and Object.py
@@ -0,0 +1,10 @@
+class Student:
+    def __init__(self, name, roll):
+        self.name = name
+        self.roll = roll
+
+    def display(self):
+        print(f"Name: {self.name}, Roll: {self.roll}")
+
+s1 = Student("Hemadevi", 101)
+s1.display()
